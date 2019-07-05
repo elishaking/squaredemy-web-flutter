60b1593c660277cb7e85c000422599956c36bedb
King Elisha <ek.chibueze@gmail.com>
2019-07-05 10:05:59 +0100

    body

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,55 @@
 import 'package:flutter_web/material.dart';
 import 'package:squaredemy_web/widgets/nav.dart';
+import 'package:squaredemy_web/widgets/text.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double contentWidth = MediaQuery.of(context).size.width / 2 - MediaQuery.of(context).size.width * 0.1;
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(45, 63, 97, 1),
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1, vertical: 20),
         child: Column(
           children: <Widget>[
-            NavBar()
+            NavBar(),
+            Row(
+              children: <Widget>[
+                Container(
+                  width: contentWidth,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      HeadlineText("The Most Productive Way to Learn"),
+                      SizedBox(height: 30,),
+                      TitleText("One sentence unique value proposition goes here "),
+                      SizedBox(height: 90,),
+                      Row(
+                        children: <Widget>[
+                          RaisedButton(
+                            child: ButtonText("Join Testing",),
+                            elevation: 0,
+                            onPressed: (){
+
+                            },
+                            color: Color.fromRGBO(34, 209, 164, 1),
+                            padding: EdgeInsets.symmetric(horizontal: 85, vertical: 30),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: contentWidth,
+                  // alignment: Alignment.centerRight,
+                  child: Image(
+                    image: AssetImage("phone.png"),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -13,3 +13,51 @@ class BodyText extends StatelessWidget {
     ),);
   }
 }
+
+class ButtonText extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  ButtonText(this.text, {this.color = Colors.white});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(this.text, 
+    style: TextStyle(
+      color: this.color,
+      fontSize: 20
+    ),);
+  }
+}
+
+class TitleText extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  TitleText(this.text, {this.color = Colors.white});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(this.text, 
+    style: TextStyle(
+      color: this.color,
+      fontSize: 27
+    ),);
+  }
+}
+
+class HeadlineText extends StatelessWidget {
+  final String text;
+
+  HeadlineText(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(this.text, 
+    style: TextStyle(
+      color: Colors.white,
+      fontSize: 60
+    ),
+    overflow: TextOverflow.clip,);
+  }
+}
