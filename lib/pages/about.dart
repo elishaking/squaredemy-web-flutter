44620406501fdea0ab80f9bfc34c2bf1ff91e234
King Elisha <ek.chibueze@gmail.com>
2019-07-11 11:42:06 +0100

    mission/vision responsive

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -9,10 +9,11 @@ import 'package:squaredemy_web/widgets/text.dart';
 class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final double contentWidth = MediaQuery.of(context).size.width / 2 - MediaQuery.of(context).size.width * 0.2;
     final double contentHeight = MediaQuery.of(context).size.height;
-    final double contactWidth = MediaQuery.of(context).size.width * 0.4;
-    final contentPadding = EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1, vertical: 20);
+    final double contentWidth = MediaQuery.of(context).size.width;
+    final double splitWidth = contentWidth / 2 - contentWidth * 0.2;
+    final double contactWidth = contentWidth * 0.4;
+    final contentPadding = EdgeInsets.symmetric(horizontal: contentWidth * 0.1, vertical: 20);
 
     return Scaffold(
       backgroundColor: ThemeColors.primary,
@@ -51,14 +52,15 @@ class AboutPage extends StatelessWidget {
 
             /// [section] [2]
             Container(
-              height: contentWidth < 1100 ? contentHeight * 1.8 : contentHeight,
+              height: contentWidth < 1100 ? contentHeight * 1.2 : contentHeight,
+              width: double.infinity,
               padding: contentPadding,
               color: ThemeColors.primaryLight,
               child: Column(
                 children: <Widget>[
                   // SizedBox(height: 100,),
                   HeadlineText("Meet the Team"),
-                  SizedBox(height: 40,),
+                  SizedBox(height: responsiveSize(contentWidth, 40),),
                   TitleText("We love teamwork. We love the idea of everyone rallying together to help us win."),
                   SizedBox(height: responsiveSize(contentWidth, 70),),
                   _buildTeamMembers(contentWidth)
@@ -77,41 +79,17 @@ class AboutPage extends StatelessWidget {
 
             /// [section] [3]
             Container(
-              height: contentHeight * 1.5,
+              height: contentWidth < 1100 ? contentHeight * 2.3 : contentHeight,
+              width: double.infinity,
               padding: contentPadding,
               child: Column(
                 children: <Widget>[
                   HeadlineText("Tough problems solved through innovative software soultions"),
-                  SizedBox(height: 60,),
+                  SizedBox(height: responsiveSize(contentWidth, 60),),
                   TitleText("We are team of tenacious professionals with broadexpertise in graphic design and implementation of responsive/modern website, mobile apps and desktop apps. Our main goal is to keep tackling major socio-economic issues facing nations of the world through innovative software solutions"),
-                  SizedBox(height: 120,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Container(
-                        width: contentWidth,
-                        child: Image(
-                          image: AssetImage("vision.png"),
-                        ),
-                      ),
-                      Container(
-                        width: contentWidth,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            HeadlineText("Mission"),
-                            SizedBox(height: 30,),
-                            TitleText("We aim to standout as the leading provider of comprehensive and innovative software platform that tackle major problems in education and financial sectors in Nigeria"),
-                            SizedBox(height: 50,),
-                            HeadlineText("Vision"),
-                            SizedBox(height: 30,),
-                            TitleText("Accelerate Africa's transition to a robust digital economy through innovative-driven software solutions"),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 120),
+                  SizedBox(height: responsiveSize(contentWidth, 100),),
+                  _buildMissionVission(splitWidth, contentWidth),
+                  SizedBox(height: responsiveSize(contentWidth, 100)),
                   RaisedButton(
                     child: ButtonText("Visit Skyblazar",),
                     elevation: 0,
@@ -250,11 +228,51 @@ class AboutPage extends StatelessWidget {
     );
   }
 
+  Widget _buildMissionVission(final double splitWidth, final double contentWidth) {
+    final double width = contentWidth < 1100 ? double.infinity : splitWidth;
+
+    List<Widget> contents = [
+      Container(
+        width: width,
+        child: Image(
+          image: AssetImage("vision.png"),
+        ),
+      ),
+      SizedBox(height: 30),
+      Container(
+        width: width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            HeadlineText("Mission"),
+            SizedBox(height: 30,),
+            TitleText("We aim to standout as the leading provider of comprehensive and innovative software platform that tackle major problems in education and financial sectors in Nigeria", textAlign: TextAlign.start,),
+            SizedBox(height: 50,),
+            HeadlineText("Vision"),
+            SizedBox(height: 30,),
+            TitleText("Accelerate Africa's transition to a robust digital economy through innovative-driven software solutions", textAlign: TextAlign.start),
+          ],
+        ),
+      )
+    ];
+
+    if(contentWidth < 1100){
+      return Column(
+        children: contents,
+      );
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: contents,
+    );
+  }
+
   Widget _buildTeamMembers(final double contentWidth) {
     List<Widget> members = [
-      TeamMember("KING ELISHA", "Founder/CEO", "king.png"),
+      TeamMember("KING ELISHA", "Founder/CEO", "king.png", contentWidth),
       SizedBox(height: 30,),
-      TeamMember("SHEDRACH ELURIHU", "Co-Founder/Designer", "shedrach.jpeg")
+      TeamMember("SHEDRACH ELURIHU", "Co-Founder/Designer", "shedrach.jpeg", contentWidth)
     ];
 
     if(contentWidth < 1100){
@@ -273,13 +291,14 @@ class TeamMember extends StatelessWidget {
   final String fullName;
   final String title;
   final String imageUrl;
+  final double contentWidth;
 
-  TeamMember(this.fullName, this.title, this.imageUrl);
+  TeamMember(this.fullName, this.title, this.imageUrl, this.contentWidth);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 50),
+      padding: EdgeInsets.symmetric(horizontal: responsiveSize(contentWidth, 50), vertical: responsiveSize(contentWidth, 50)),
       // width: 200,
       decoration: BoxDecoration(
         color: Colors.white,
@@ -289,11 +308,11 @@ class TeamMember extends StatelessWidget {
         children: <Widget>[
           CircleAvatar(
             backgroundImage: AssetImage(imageUrl),
-            radius: 100,
+            radius: responsiveSize(contentWidth, 100),
           ),
-          SizedBox(height: 40,),
+          SizedBox(height: responsiveSize(contentWidth, 40),),
           TitleText(fullName, color: ThemeColors.primary,),
-          SizedBox(height: 20,),
+          SizedBox(height: responsiveSize(contentWidth, 20),),
           BodyText(title, color: ThemeColors.primary,)
         ],
       ),
