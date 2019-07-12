39f0c0ed9da52d102f812c3e7fe6355d51c5df3b
King Elisha <ek.chibueze@gmail.com>
2019-07-12 12:19:24 +0100

    subscribe form

--- lib/widgets/forms/subscribe_form.dart
+++ lib/widgets/forms/subscribe_form.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter_web/material.dart';
 import 'package:squaredemy_web/global/styles.dart';
+import 'package:squaredemy_web/models/user.dart';
 import 'package:squaredemy_web/widgets/text.dart';
+
+import 'package:http/http.dart' as http;
 
 class SubscribeForm extends StatefulWidget {
   final double contentWidth;
@@ -14,6 +19,11 @@ class SubscribeForm extends StatefulWidget {
 }
 
 class _SubscribeFormState extends State<SubscribeForm> {
+  GlobalKey<FormState> _subscribeFormKey = GlobalKey<FormState>();
+  User _user = User();
+
+  bool _loading = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -51,8 +61,14 @@ class _SubscribeFormState extends State<SubscribeForm> {
             )
           ),
           filled: true,
-          fillColor: ThemeColors.primary
+          fillColor: ThemeColors.primary,
         ),
+        validator: (String value){
+          if(value.isEmpty) return "Please enter your email";
+        },
+        onSaved: (String value){
+          _user.email = value;
+        },
       ),
     );
 
@@ -60,6 +76,25 @@ class _SubscribeFormState extends State<SubscribeForm> {
       child: ButtonText("Subscribe",),
       elevation: 0,
       onPressed: (){
+        _subscribeFormKey.currentState.save();
+        
+        _user.name = "";
+        _user.isTester = false;
+
+        setState(() {
+          _loading = true; 
+        });
+        // print(jsonEncode(_user.toFirestoreMap()));
+        http.post("https://firestore.googleapis.com/v1/projects/squaredemy/databases/(default)/documents/users?documentId=${_user.email}",
+        // headers: {
+        //   "Content-Type": "application/json"
+        // },
+        body: jsonEncode(_user.toFirestoreMap())).then((http.Response response) {
+          setState(() {
+            _loading = false; 
+          });
+          showResponseDialog(response, context);
+        });
       },
       color: ThemeColors.primaryButton,
       padding: buttonPadding,
@@ -88,5 +123,25 @@ class _SubscribeFormState extends State<SubscribeForm> {
         subscribeButton
       ],
     );
+  }
+
+  void showResponseDialog(http.Response response, BuildContext context) {
+    if(jsonDecode(response.body).contains("name")){
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: Text("Welcome Onboard"),
+          content: Text("Please check your email for the next step"),
+        )
+      );
+    } else{
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: Text("Something went wrong"),
+          content: Text("Please try again"),
+        )
+      );
+    }
   }
 }
